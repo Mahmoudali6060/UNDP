60248980eb0d0dfd2f87be88f8a8e75eb27d05d3
Mohamed Roshdy <M.roshdy@aliaict.com>
2022-07-30 15:53:31 +0200

    resolve login page after logout

--- UNDP-app/src/app/layout/components/side-menu/side-menu.component.ts
+++ UNDP-app/src/app/layout/components/side-menu/side-menu.component.ts
@@ -12,7 +12,8 @@ export class SideMenuComponent {
   serverUrl:string;
   constructor(
     public authService: AuthService,
-    private _configService: ConfigService
+    private _configService: ConfigService,
+    private router:Router
 
    ) {
     this.serverUrl = _configService.getServerUrl();
@@ -21,5 +22,7 @@ export class SideMenuComponent {
 
   public logOut() {
     this.authService.logOut();
+    this.router.navigate(["login"]);
+
   }
 }
