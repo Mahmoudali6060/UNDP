1db2ac71aaeb3ed4fca6972560690d7fd2b0671c
Mohamed Roshdy <M.roshdy@aliaict.com>
2022-08-02 23:12:56 +0200

    test add request form  html only in landing page

--- UNDP-app/src/app/modules/home/home.module.ts
+++ UNDP-app/src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FleetManagementModule } from '../fleet-management/fleet-management.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FleetManagementModule
   ],exports:[
     LandingPageComponent
   ]
