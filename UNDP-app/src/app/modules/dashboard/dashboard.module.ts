254b2ab20d6a275b62cc16822f528b8664f65541
Mohamed Roshdy <M.roshdy@aliaict.com>
2022-08-14 09:04:30 +0200

    redesign status

--- UNDP-app/src/app/modules/dashboard/dashboard.module.ts
+++ UNDP-app/src/app/modules/dashboard/dashboard.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard.component';
 import { SharedModule } from '../../shared/shared.module';
@@ -14,7 +14,10 @@ import { AuthGuardService } from 'src/app/shared/guards/auth-guard.service';
   ],
   providers: [
     AuthGuardService
-  ]
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 
 export class DashboardModule {
