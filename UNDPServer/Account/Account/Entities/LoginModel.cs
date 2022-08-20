062dd634d0381bc31b32561c286beed69f9d0685
m.salman <m.salman@aliaict.com>
2022-08-20 13:02:13 +0200

    Add Token Property to LoginModel

--- UNDPServer/Account/Account/Entities/LoginModel.cs
+++ UNDPServer/Account/Account/Entities/LoginModel.cs
@@ -8,5 +8,7 @@ namespace Account.Entities
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string DeviceId { get; set; }
+
     }
 }
